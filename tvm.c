5c8a26f1a8555aba30f40c6b575a3f67f88da767
Tyrone Parsons <Tyrone.Parsons@signature.co.za>
2020-09-27 21:38:33 +0200

    Initial folder set up.

---
+++ tvm.c
@@ -0,0 +1,21 @@
+/* tvm.c */
+#include <stdint.h>
+#include <stdio.h>
+
+/* 16-bit memory */
+uint16_t memory[UINT16_MAX + 1];
+
+/* loads a state image from a give file */
+void load_image_from_file(const char* path)
+{
+    
+}
+
+/* main entry point */
+int main(int argc, const char* argv[])
+{
+    /* start the vm */
+    int running = 1;
+    
+    printf("%i", UINT16_MAX);
+}
